5c957519c39761cc72ef83aac74364400ee3bbed
LiadDamhuis <li.ad.28@hotmail.com>
2019-04-04 13:13:29 +0200

    BasicPoint&ClickInteractions
    Scene switch by button and object

---
+++ PointClick/Assets/Scripts/SceneSwitcherObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcherObject : MonoBehaviour
+{
+    public int num;
+   
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(num);
+        Debug.Log("Scene changed to " + num);
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+
+            if (hit.collider.gameObject.tag == "Switch")
+              {
+                    SceneManager.LoadScene(num);
+                    Debug.Log("Scene changed to " + num);
+              }
+           }
+        }
+    }
+}
