d23d63b8dce2d8cfa12c7c705de5b03807f43343
LiadDamhuis <li.ad.28@hotmail.com>
2019-04-05 14:22:32 +0200

    DisplayImageOn/Off
    Next: Fade in/out

---
+++ PointClick/Assets/Scripts/DisplayImage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayImage : MonoBehaviour
+{
+
+
+
+    public GameObject displayCanvas;
+ 
+
+    public void ShowDisplayImage()
+    {
+        displayCanvas.SetActive(false);
+    }
+
+
+    private void OnMouseEnter()
+    {
+        displayCanvas.SetActive(true);
+
+    }
+
+    private void OnMouseExit()
+    {
+        displayCanvas.SetActive(false);
+    }
+
+
+}
