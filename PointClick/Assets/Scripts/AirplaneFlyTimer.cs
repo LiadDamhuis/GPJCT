22ef7b0a809e984a7c0d2d64cbaecde6856fa88e
LiadDamhuis <li.ad.28@hotmail.com>
2019-06-17 18:19:27 +0200

    Update

---
+++ PointClick/Assets/Scripts/AirplaneFlyTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneFlyTimer : MonoBehaviour
+{
+    
+
+AudioSource myAudio;
+
+   void Start()
+  {
+      myAudio = GetComponent<AudioSource>();
+       myAudio.PlayDelayed(20.0f);
+   }
+
+
+  void Update()
+   {
+
+  }
+}
